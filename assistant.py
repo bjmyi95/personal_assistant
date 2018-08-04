13ada6a0285e1690b398bfdd36c6ed9835aa3a33
Benjamin Yi <by2212@columbia.edu>
2018-08-04 17:48:12 -0400

    improve regEx, add index.html and moduel.js

--- assistant.py
+++ assistant.py
@@ -1,8 +1,13 @@
 import sys
 import random
+import re     
 
-inputDict = {'Asking feeling':["How are you?", "What's up?", "How you doing?"]};
-outputDict = {"Asking feeling":["I'm good, how about you?", "I'm great, what about you?", "I'm not feeling so well, but how about you?"]};
+afq1 = r"\wow are you\??" #How are you?
+afq2 = r"\what(s |'s | is )up\??" #What's up?
+afq3 = r"\wow( are | )you doing\??" #How you doing?
+afq4 = r"(^[Ww]hat(s |'s | is )up\??$)|(^[Hh]ow (((are you)( doing)?)|(you doing))\??$)" 
+inputDict = {'Asking feeling':[afq4]};
+outputDict = {"Asking feeling":["I'm good.", "I'm great.", "I'm not feeling so well."]};
 
 def runPersonalAssistant():
 	while True:
@@ -15,15 +20,22 @@ def runPersonalAssistant():
 		if foundKey:
 			for key in outputDict.keys():
 				if key == foundKey:
-					print (random.choice(outputDict[key]));
+					if re.search(afq1, response, flags=0):
+						print (random.choice(outputDict[key])+' How are you?');
+					elif re.search(afq2, response, flags=0):
+						print (random.choice(outputDict[key])+" What's up?");
+					elif re.search(afq3, response, flags=0):
+						print (random.choice(outputDict[key])+" How are you doing?");
 		else:
 			print ("I don't understand")
 
 def findKey(input, dict):
 	for key, values in dict.items():
 		for value in values:
-			if input == value: 
+			if re.search(value, input, flags=0):
 				return key;
+			#if input == value: 
+			#	return key;
 #		
 #		if input == value:
 #			print (key, value);
